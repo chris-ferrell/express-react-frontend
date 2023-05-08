280f951bf809851bae7a375c32fee97ff5d20889
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 19:59:08 -0400

    updated

---
+++ src/pages/People.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './People.css'
+		  
+function People() {
+    return (
+		<div className="people">
+					  inside people
+		</div>
+	)
+}
+		  
+export default People
+		  
