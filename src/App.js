4d42c7af34a0301ed4fe6dae7224a182966861db
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 19:58:37 -0400

    App.js updated with header and nmain

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React, {Component} from 'react';
+import People from './pages/People'
 function App() {
   return (
     <div className="App">
-     
-    {/* code here */}
-
+      App.js
+    <header>
+       <h1>People App</h1>
+    </header>
+    <main>
+      <People />
+    </main>
     </div>
   );
 }
