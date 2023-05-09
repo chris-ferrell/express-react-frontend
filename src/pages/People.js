cb3467713a6cd6ccc26c02f07fc656bb327e122b
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 20:24:22 -0400

    setting up peopleList callback

--- src/pages/People.js
+++ src/pages/People.js
@@ -19,17 +19,28 @@ function People() {
 				let responseData = await fetch(URL);
 				let allPeople = await responseData.json();
 				console.log(allPeople);
+				setPeople(allPeople);
 			} catch (error) {
 
-			}
+			}; // end of funciton
 		}
 	},[])
+
+	let peopleList = people.map(person => {
+		return(
+			<div key={}>
+				<h3>{person.name}</h3>
+				<h3>{person.title}</h3>
+				<img src={} alt='avatar' />
+			</div>
+		)
+	})
 
     return (
 		<div className='people'>
 			<h2>All The People </h2>
 			{/* render the people */}
-
+			<pre>{people[0].name}</pre>
 		</div>
 	)
 }
