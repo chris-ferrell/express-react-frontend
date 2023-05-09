e799dedf9aecfbb3fc0d7f4811633be8e0a46fc2
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 21:29:52 -0400

    inputs created

--- src/pages/NewPerson.js
+++ src/pages/NewPerson.js
@@ -6,15 +6,36 @@ function NewPerson() {
   const [titleState, setTitleState] = useState("");
   const [imageState, setImageState] = useState("");
 
-  const onChangeHandle = (e, setValue) => {
-	console.log(e.target.value);
-	setValue(e.target.value); // this represents any state updater (setName) that we passed in 
-  }; // end of function
+  //Here we are making a dynamic onChangeHandler that'll accept a state updater
+  const onChangeHandler = (e, setValue) => {
+    console.log(e.target.value);
+    setValue(e.target.value); //this represents any state updater (setName) that we passed in
+  }; //end of func
 
   return (
     <div className="newperson">
       <form>
-        <input type="text" value={nameState} name="name" placeholder="name" />
+        <input
+          type="text"
+          value={nameState}
+          name="name"
+          placeholder="name"
+          onChange={(e) => onChangeHandler(e, setNameState)}
+        />
+        <input
+          type="text"
+          value={imageState}
+          name="image"
+          placeholder="image URL"
+          onChange={(e) => onChangeHandler(e, setImageState)}
+        />
+        <input
+          type="text"
+          value={titleState}
+          name="title"
+          placeholder="title"
+          onChange={(e) => onChangeHandler(e, setTitleState)}
+        />
 
         <input type="submit" value="Create a Person" />
       </form>
