86eae1dd142d7facac47b77075644d8b41e4ee0f
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 20:12:15 -0400

    useState initialized

--- src/pages/People.js
+++ src/pages/People.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 import './People.css'
 		  
 function People() {
+	
+	const [people, setPeople] = useState([]);
+
+
+
 	const URL = 'https://people-api-qn7s.onrender.com/people/'
 	
 	useEffect(() => {
-		
+		// useeffect will render once when the compon is mounted.
+    	// if array dep. is left empty, it will only execute it's code once. 
+		console.log('UseEffect ran');
+		const fetchPeople = async () => {
+			try {
+				let responseData = await fetch(URL);
+				let allPeople = await responseData.json();
+				console.log(allPeople);
+			} catch (error) {
+
+			}
+		}
 	},[])
 
     return (
