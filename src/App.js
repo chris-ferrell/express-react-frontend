b5a802ce9fcf36b27fd5a566c39701ec3300c72f
unknown-bytes <unknown.develops@gmail.com>
2023-05-08 21:12:09 -0400

    Routes added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './App.css';
 import React, {Component} from 'react';
 import People from './pages/People'
 function App() {
+
+  routes = (
+  <Routes>
+
+    <Route exact = { true } path = '/' element={ <People /> } />
+    <Route Path='/people/new' element={<NewPerson />} />
+
+  </Routes> 
+  );
+
   return (
     <div className="App">
       App.js
